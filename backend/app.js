cbb4ed43d26387cf44f9e700927e4eb4496087b0
Vasya Dmitrov <vd-boy400@yandex.ru>
2023-08-05 23:07:36 +0300

    проверка2

--- backend/app.js
+++ backend/app.js
@@ -2,71 +2,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const mongoose = require('mongoose');
-const {
-  celebrate, Joi, errors, Segments,
-} = require('celebrate');
+const { errors } = require('celebrate');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-const { login, createUser } = require('./controllers/users');
-const NotFoundError = require('./errors/NotFoundError');
+const router = require('./routes');
+const cors = require('./middlewares/cors');
 
 const app = express();
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
-const usersRouter = require('./routes/users');
-const cardsRouter = require('./routes/cards');
-const auth = require('./middlewares/auth');
 const errorResponse = require('./middlewares/errorResponse');
 
-const regex = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/;
-
+app.use(cors);
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(requestLogger);
 
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
-
-// без проверки авторизации
-app.post(
-  '/signin',
-  celebrate({
-    [Segments.BODY]: Joi.object().keys({
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
-    }),
-  }),
-  login,
-);
-
-app.post(
-  '/signup',
-  celebrate({
-    [Segments.BODY]: Joi.object().keys({
-      name: Joi.string().min(2).max(30),
-      email: Joi.string().required().email(),
-      password: Joi.string().required(),
-      about: Joi.string().min(2).max(30),
-      avatar: Joi.string()
-        .default(
-          'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
-        )
-        .regex(regex),
-    }),
-  }),
-  createUser,
-);
-
-// с проверкой авторизации
-app.use(auth);
-app.use('/users', usersRouter);
-app.use('/cards', cardsRouter);
-app.use('/*', (req, res, next) => {
-  next(new NotFoundError(' Страница не найдена'));
-});
+app.use('/api', router);
+// app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(errorLogger);
 app.use(errors());
---
+++ backend/routes/main.js
@@ -0,0 +1,56 @@
+const router = require('express').Router();
+const {
+  celebrate, Joi, Segments,
+} = require('celebrate');
+const { login, createUser } = require('./controllers/users');
+const NotFoundError = require('./errors/NotFoundError');
+
+const usersRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards');
+const auth = require('./middlewares/auth');
+
+const regex = /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/;
+
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
+// без проверки авторизации
+router.post(
+  '/signin',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
+  }),
+  login,
+);
+
+router.post(
+  '/signup',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().min(2).max(30),
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string()
+        .default(
+          'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+        )
+        .regex(regex),
+    }),
+  }),
+  createUser,
+);
+
+// с проверкой авторизации
+router.use(auth);
+router.use('/users', usersRouter);
+router.use('/cards', cardsRouter);
+router.use('/*', (req, res, next) => {
+  next(new NotFoundError(' Страница не найдена'));
+});
