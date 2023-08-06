0e454071d8ea513642e5bc5c06586a085203adca
Vasya Dmitrov <vd-boy400@yandex.ru>
2023-08-06 10:37:51 +0300

    проверка_1

--- frontend/src/utils/authApi.js
+++ frontend/src/utils/authApi.js
@@ -1,4 +1,4 @@
-export const baseURL = 'http://mesto-dmitrov.nomoreparties.co/api';
+export const baseURL = 'https://mesto-dmitrov.nomoreparties.co/api';
 
 export function checkResponse(res) {
   return res.ok ? res.json() : Promise.reject(`Ошибка ${res.status}`);
