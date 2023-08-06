240afdff2c07872b1abadef188d6c8389460b9ef
Vasya Dmitrov <vd-boy400@yandex.ru>
2023-08-06 20:07:31 +0300

    проверка_1

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -148,7 +148,7 @@ function App() {
     const isLiked = card.likes.some((i) => i === currentUser._id);
 
     // Отправляем запрос в API и получаем обновлённые данные карточки
-    if(isLiked) {api
+    if(!isLiked) {api
       .displayNumberLike(card._id)
     .then((newCard) => {
         setCards((state) =>
