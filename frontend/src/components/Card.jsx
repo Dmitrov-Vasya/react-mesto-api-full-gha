8d9cff024345e54ecb09d5eb7a9e18ead67f0f4b
Vasya Dmitrov <vd-boy400@yandex.ru>
2023-08-06 17:51:17 +0300

    проверка_1

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -8,7 +8,7 @@ function Card({ card, onCardClick, onCardLike, onCardDelete }) {
   const isOwn = card.owner._id === currentUser._id;
 
   // Определяем, есть ли у карточки лайк, поставленный текущим пользователем
-  const isLiked = card.likes.some((i) => i._id === currentUser._id);
+  const isLiked = card.likes.some((i) => i === currentUser);
 
   // Создаём переменную, которую после зададим в `className` для кнопки лайка
   const cardLikeButtonClassName = `place__button-like ${
