2c8a4df0cf4826ebfbeecd522a4b6d6ed04d4b73
Vasya Dmitrov <vd-boy400@yandex.ru>
2023-08-06 13:04:27 +0300

    проверка_1

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(requestLogger);
 
-app.use('/api', router);
+app.use('/', router);
 // app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(errorLogger);
