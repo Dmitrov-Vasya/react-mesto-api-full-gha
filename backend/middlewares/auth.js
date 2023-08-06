4f5a7b44cb26beb1789a89482db2b51731790cab
Vasya Dmitrov <vd-boy400@yandex.ru>
2023-08-06 15:41:52 +0300

    проверка_1

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,12 +2,13 @@ const jwt = require('jsonwebtoken');
 const AuthorizationError = require('../errors/AuthorizationError');
 
 module.exports = (req, res, next) => {
+  const { NODE_ENV, JWT_SECRET } = process.env;
   try {
     const token = req.cookies.access_token;
     if (!token) {
       return next(new AuthorizationError('Необходима авторизация'));
     }
-    const reqUser = jwt.verify(token, 'bUKN7CkRUJuzC4hJhevpz9m3');
+    const reqUser = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'bUKN7CkRUJuzC4hJhevpz9m3');
     req.user = reqUser;
     return next();
   } catch (err) {
